dac908f87522b43981cad9d02c40935ae7eae3de
Ashley <ashley21_05@hotmail.co.uk>
2026-02-19 16:02:22 +0000

    First commit

---
+++ RPGProject/RPGApp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RpgappConfig(AppConfig):
+    name = 'RPGApp'
---
+++ RPGProject/RPGProject/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for RPGProject project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RPGProject.settings')
+
+application = get_asgi_application()
