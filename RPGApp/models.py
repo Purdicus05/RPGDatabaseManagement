bf15f107a6e1415e1b8482f7e838d8dd0dd82cb9
Ashley <ashley21_05@hotmail.co.uk>
2026-03-05 11:47:46 +0000

    Added info to models.py

--- RPGApp/models.py
+++ RPGApp/models.py
@@ -1,3 +1,70 @@
 from django.db import models
 
 # Create your models here.
+
+class players (models.Model):
+    player_ID = models.IntegerField()
+    player_firstName = models.CharField(max_length=255)
+    player_surname = models.CharField(max_length=255)
+    player_username = models.CharField(max_length=255)
+
+class spells (models.Model):
+    spell_ID = models.IntegerField()
+    spell_name = models.CharField(max_length=32)
+    spell_school = models.CharField(max_length=16)
+    spell_damage = models.IntegerField()
+    spell_dmgType = models.CharField(max_length=32)
+    spell_level = models.IntegerField()
+
+class races (models.Model):
+    race_name = models.CharField(max_length=32)
+    race_darkVision = models.BooleanField()
+    race_strMod = models.IntegerField()
+    race_charMod = models.IntegerField()
+    race_wisMod = models.IntegerField()
+    race_intMod = models.IntegerField()
+    race_conMod = models.IntegerField()
+    race_dexMod = models.IntegerField()
+
+class classes (models.Model):
+    class_name = models.CharField(max_length=32)
+    class_spellSlots= models.IntegerField()
+    class_strMod = models.IntegerField()
+    class_charMod = models.IntegerField()
+    class_wisMod = models.IntegerField()
+    class_intMod = models.IntegerField()
+    class_conMod = models.IntegerField()
+    class_dexMod = models.IntegerField()
+
+class armour (models.Model):
+    armour_ID = models.IntegerField()
+    armour_name = models.CharField(max_length=64)
+    armour_type = models.CharField(max_length=8)
+    armour_armourClass = models.IntegerField()
+    armour_weight = models.IntegerField()
+
+class characters(models.Model):
+    character_ID = models.IntegerField()
+    character_name = models.CharField(max_length=24)
+    character_class = models.CharField(max_length=16)
+    character_race = models.CharField(max_length=32)
+    character_armourID = models.IntegerField()
+    character_weaponID = models.IntegerField()
+    character_spellID = models.IntegerField()
+    character_hitPoints = models.IntegerField()
+    character_level = models.IntegerField()
+    character_strength = models.IntegerField()
+    character_intelligence = models.IntegerField()
+    character_dexterity = models.IntegerField()
+    character_constitution = models.IntegerField()
+    character_wisdom = models.IntegerField()
+    character_playerID = models.IntegerField()
+
+class npc (models.Model):
+    npc_ID = models.IntegerField()
+    npc_type = models.CharField(max_length=32)
+    npc_hp = models.IntegerField()
+    npc_ac = models.IntegerField()
+    npc_weaponID = models.IntegerField()
+    npc_spellID = models.IntegerField()
+    npc_race = models.CharField(max_length=32)
