6cc6eaf231f5e0d2092585bc620e37745ef383d7
Ashley <ashley21_05@hotmail.co.uk>
2026-03-06 14:02:07 +0000

    Added models to admin.py

--- RPGApp/admin.py
+++ RPGApp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Weapons)
+admin.site.register(Armour)
+admin.site.register(Characters)
+admin.site.register(Classes)
+admin.site.register(Npc)
+admin.site.register(Players)
+admin.site.register(Races)
+admin.site.register(Spells)
