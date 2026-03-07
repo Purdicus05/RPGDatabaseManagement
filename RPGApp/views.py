1005fb620648db2ffe641839988c82eae5d58763
Ashley <ashley21_05@hotmail.co.uk>
2026-03-07 18:55:01 +0000

    Loads of html

--- RPGApp/views.py
+++ RPGApp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 from .forms import *
+from .models import *
 from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='/')
@@ -11,29 +12,36 @@ def home (request):
     return render(request, 'home.html')
 
 def characters(request):
-    return render(request, 'characters.html')
+    character_list = Characters.objects.select_related("character_playerid").all().order_by("character_name")
+    return render(request, 'characters.html', {'Characters': character_list})
 
 def npcs(request):
-    return render(request, 'npc.html')
+    npc_list = Npc.objects.select_related('npc_weaponid').select_related('npc_spellid').all().order_by("npc_type")
+    return render(request, 'npc.html', {'npcs': npc_list})
 
 def players(request):
-    return render(request, 'players.html')
+    player_list = Players.objects.all().order_by("player_surname")
+    return render(request, 'players.html', {'Players': player_list})
 
 def weapons(request):
-    return render(request, 'weapons.html')
+    weapon_list = Weapons.objects.all().order_by("weapon_name")
+    return render(request, 'weapons.html', {'Weapons': weapon_list})
 
 def armour(request):
-    return render(request, 'armour.html')
+    armour_list = Armour.objects.all().order_by("armour_name")
+    return render(request, 'armour.html', {'Armour': armour_list})
 
 def spells(request):
-    return render(request, 'spells.html')
+    spell_list = Spells.objects.all().order_by("spell_name")
+    return render(request, 'spells.html', {'Spells': spell_list})
 
 def races(request):
-    return render(request, 'races.html')
+    race_list = Races.objects.all().order_by("race_name")
+    return render(request, 'races.html', {'Races': race_list})
 
 def classes(request):
-    return render(request, 'classes.html')
-
+    class_list = Classes.objects.all().order_by("class_name")
+    return render(request, 'classes.html', {'Classes': class_list})
 
 
 def login (request):
@@ -44,7 +52,62 @@ def player_add(request):
         form = PlayerForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect("home")
+            return redirect("players")
     else:
         form = PlayerForm()
     return render(request, "player_add.html", {"form": form})
+
+def player_update(request):
+    return render(request, 'player_update.html')
+def player_delete(request):
+    return render(request, 'player_delete.html')
+
+def character_add(request):
+
+    return render(request, 'character_add.html')
+
+def character_delete(request):
+    return render(request, 'character_delete.html')
+
+def character_update(request):
+    return render(request, 'character_update.html')
+
+def npc_delete(request):
+    return render(request, 'npc_delete.html')
+def npc_update(request):
+    return render(request, 'npc_update.html')
+
+def weapon_delete(request):
+    return render(request, 'weapon_delete.html')
+def weapon_update(request):
+    return render(request, 'weapon_update.html')
+def weapon_add(request):
+    return render(request, 'weapon_add.html')
+
+def armour_delete(request):
+    return render(request, 'armour_delete.html')
+def armour_update(request):
+    return render(request, 'armour_update.html')
+def armour_add(request):
+    return render(request, 'armour_add.html')
+
+def spell_delete(request):
+    return render(request, 'spell_delete.html')
+def spell_update(request):
+    return render(request, 'spell_update.html')
+def spell_add(request):
+    return render(request, 'spell_add.html')
+
+def class_delete(request):
+    return render(request, 'class_delete.html')
+def class_update(request):
+    return render(request, 'class_update.html')
+def class_add(request):
+    return render(request, 'class_add.html')
+
+def race_delete(request):
+    return render(request, 'race_delete.html')
+def race_update(request):
+    return render(request, 'race_update.html')
+def race_add(request):
+    return render(request, 'race_add.html')
--- RPGProject/urls.py
+++ RPGProject/urls.py
@@ -31,7 +31,36 @@ urlpatterns = [
     path('armour/', views.armour, name="armour"),
     path('weapons/', views.weapons, name="weapons"),
     path('spells/', views.spells, name="spells"),
+    path('classes/', views.classes, name="classes"),
 
     path('player_add/', views.player_add, name="player_add"),
+    path('character_add/', views.character_add, name="character_add"),
+    path('character/update/<int:id>/', views.character_update, name="Character_update"),
+    path('character/delete/<int:id>/', views.character_delete, name="Character_delete"),
+    path('player/update/<int:id>/', views.player_update, name="player_update"),
+    path('player/delete/<int:id>/', views.player_delete, name="player_delete"),
 
+    path('npc/update/<int:id>/', views.npc_update, name="npc_update"),
+    path('npc/delete/<int:id>/',views.npc_delete,name="npc_delete"),
+
+    path('weapon/update/<int:id>/', views.weapon_update, name="weapon_update"),
+    path('weapon/delete/<int:id>/',views.weapon_delete,name="weapon_delete"),
+    path('weapon_add',views.weapon_add, name="weapon_add"),
+
+    path('armour/update/int<id>/', views.armour_update, name="armour_update"),
+    path('armour/delete/<int:id>/',views.armour_delete,name="armour_delete"),
+    path('armour_add',views.armour_add, name="armour_add"),
+
+    path('spell/update/int<id>/', views.spell_update, name="spell_update"),
+    path('spell/delete/<int:id>/',views.spell_delete,name="spell_delete"),
+    path('spell_add',views.spell_add, name="spell_add"),
+
+    path('class/update/str<id>/', views.class_update, name="class_update"),
+    path('class/delete/<str:id>/',views.class_delete,name="class_delete"),
+    path('class_add',views.class_add, name="class_add"),
+
+    path('race/update/str<id>/', views.race_update, name="race_update"),
+    path('race/delete/<str:id>/',views.race_delete,name="race_delete"),
+    path('race_add',views.race_add, name="race_add"),
+    path('races/',views.races, name="races"),
 ]
